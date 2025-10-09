c356e4be33200807cc0c3690441f6486e178eb78
Seweryn Mlodawski <seweryn.mlodawski@gmail.com>
2025-10-09 19:27:03 +0200

    Wszystko zakomentowałem

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-kaufen_list = {"piekarnii": ["chleb", "pączek","bułki"],
-               "warzywniaku":["marchew", "seler", "rukola"]}
-
-total_products = 0
-
-for store, items in kaufen_list.items():
-    print(f"Gdy będę w {store}, kupię: {items}")
-
-    # Ale duzymi literami
-print("\nAle duzymi literami:\n")
-
-for store, items in kaufen_list.items():
-    store_L = store.capitalize()
-    items_L = [zakupy.capitalize() for zakupy in items]
-    total_products += len(items)
-
-    print(f"Gdy będę w {store_L}, kupię: {items_L}")
-    
-print(f"\nŁącznie kupię {total_products} produktów.")
+#kaufen_list = {"piekarnii": ["chleb", "pączek","bułki"],
+#               "warzywniaku":["marchew", "seler", "rukola"]}
+#
+#total_products = 0
+#
+#for store, items in kaufen_list.items():
+#    print(f"Gdy będę w {store}, kupię: {items}")
+#
+#    # Ale duzymi literami
+#print("\nAle duzymi literami:\n")
+#
+#for store, items in kaufen_list.items():
+#    store_L = store.capitalize()
+#    items_L = [zakupy.capitalize() for zakupy in items]
+#    total_products += len(items)
+#
+#    print(f"Gdy będę w {store_L}, kupię: {items_L}")
+#    
+#print(f"\nŁącznie kupię {total_products} produktów.")
 #A tu dodałem komentarz na nowej gałęzi
