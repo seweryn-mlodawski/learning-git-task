e4cb9d73420057edb21880f9e0b910592aa2070a
Seweryn Mlodawski <seweryn.mlodawski@gmail.com>
2025-10-09 11:37:50 +0200

    Add loop

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 kaufen_list = {"piekarnii": ["chleb", "pączek","bułki"],
                "warzywniaku":["marchew", "seler", "rukola"]}
+
+total_products = 0
+
+for store, items in kaufen_list.items():
+    print(f"Gdy będę w {store}, kupię: {items}")
