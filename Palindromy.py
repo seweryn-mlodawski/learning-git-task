dc873e98821e76b6afaf44ad12146279fe40c839
SewerKomp <seweryn.mlodawski@gmail.com>
2025-10-18 21:57:11 +0200

    Add prints

--- Palindromy.py
+++ Palindromy.py
@@ -7,12 +7,27 @@ reversed_word = word[::-1] #odwracamy slowo
 print(f"Słowo: {word}")
 print(f"Odwrócone słowo: {reversed_word}")
 
+
+test_words = [
+    "Kajak",
+    "A to kanapa pana Kota",
+    "Python",
+    "Anna",
+    "Kobyła ma mały bok!",
+    "To nie jest palindrom"
+]
+print(f"Testowane słowa to: {test_words}")
 def is_palindrome(word):
-    new_word = "" # tutaj bedziemy trzymac slowo
-    for char in word:
-        if char.isalnum(): #sprawdzamy czy znak jest alfanumeryczny
-            new_word += char
-    reversed_word = new_word[::-1] #odwracamy slowo
-    return new_word == reversed_word
-# testujemy funkcje
-test_words = ["Kajak", "A to kanapa pana Kota", "Python", "Anna", "Kobyła ma mały bok!", "To nie jest palindrom"]
+    new_word = "" #tu będziemy składać słowo
+    for char in word: # przechodzimy przez każdą literę w słowie
+        if char.isalnum(): #sprawdzamy czy znak jest alfanumeryczny (litera lub cyfra)
+            new_word += char.lower()  # zamiana na małe litery, by "A" i "a" były równe
+    reversed_word = new_word[::-1] #odwracamy słowo
+    return new_word == reversed_word # porównujemy oryginalne słowo z odwróconym
+
+print("\n--- Testowanie funkcji is_palindrome ---\n")
+for test_word in test_words:
+    if is_palindrome(test_word):
+        print(f'"{test_word}" jest palindromem.\n')
+    else:
+        print(f'"{test_word}" nie jest palindromem.\n')
