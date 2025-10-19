4187963932d3634222d6459117f94c063a19564a
SewerKomp <seweryn.mlodawski@gmail.com>
2025-10-19 13:57:42 +0200

    Add help one more time

--- Palindromy.py
+++ Palindromy.py
@@ -1,7 +1,7 @@
 #zadanie palindromy
 #plan:
 #1. funkcja sprawdzajaca czy slowo jest palindromem
-#na razie to slowo bedzie wpiswywane na sztywno w kodzie
+#na razie tekst bedzie wpiswywany na sztywno w kodzie
 
 test_words = [
     "Kajak",
@@ -11,14 +11,22 @@ test_words = [
     "Kobyła ma mały bok!",
     "To nie jest palindrom"
 ]
-print(f"Testowane słowa to: {test_words}")
+print(f"\nTestowane słowa to: {test_words}")
 def is_palindrome(word):
-    new_word = "" #tu będziemy składać słowo
-    for char in word: # przechodzimy przez każdą literę w słowie
-        if char.isalnum(): #sprawdzamy czy znak jest alfanumeryczny (litera lub cyfra)
-            new_word += char.lower()  # zamiana na małe litery, by "A" i "a" były równe
-    reversed_word = new_word[::-1] #odwracamy słowo (zwraca TRUE albo FALSE)
-    return new_word == reversed_word # porównujemy oryginalne słowo z odwróconym
+"""
+Funkcja sprawdza, czy tekst jest palindromem,
+ignoruje wielkość liter oraz znaki niebędące literami/cyframi.
+Argumenty:
+    word (str): tekst do sprawdzenia (może zawierać spacje, interpunkcję, cyfry)
+Zwraca:
+    bool: TRUE jeśli tekst jest palindromem, FALSE jeśli nie jest
+"""
+    new_word = ""                       #tu będziemy składać słowo
+    for char in word:                   # przechodzimy przez każdą literę w słowie
+        if char.isalnum():              #sprawdzamy czy znak jest alfanumeryczny (litera lub cyfra) - dzięki temu zignorujemy wykrzykniki, spacje itp.
+            new_word += char.lower()    # zamiana na małe litery, by "A" i "a" były równe
+    reversed_word = new_word[::-1]      #odwracamy słowo 
+    return new_word == reversed_word    # porównujemy oryginalne słowo z odwróconym
 
 print("\n--- Testowanie funkcji is_palindrome ---\n")
 for test_word in test_words:
